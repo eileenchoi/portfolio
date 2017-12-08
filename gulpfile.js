e1b3aa537fb4ef5ab8f090633ab001510f259de4
Eileen choi <eileen.h.choi@gmail.com>
2017-12-08 00:18:54 -0500

    up to section skills

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-'use strict'
 const gulp = require('gulp');
+const plumber = require('gulp-plumber');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
@@ -7,33 +7,29 @@ const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 
-
-
 gulp.task('styles', () => {
-    return gulp.src('./*.scss')
+    return gulp.src('./dev/styles/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(concat('styles.css'))
-        .pipe(gulp.dest('.'))
+        .pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./public/styles/'))
         .pipe(reload({ stream: true }));
 });
 
-
 gulp.task('watch', () => {
-    gulp.watch('./dev/scripts/*.js', ['scripts']);
-    gulp.watch('./*.scss', ['styles']);
+    gulp.watch('./dev/styles/**/*.scss', ['styles']);
+    gulp.watch('./dev/scripts/main.js', ['scripts']);
     gulp.watch('*.html', reload);
 });
 
 gulp.task('scripts', () => {
-    return gulp.src('./dev/scripts/main.js')
+    gulp.src('./dev/scripts/main.js')
         .pipe(babel({
             presets: ['env']
         }))
         .pipe(gulp.dest('./public/scripts'))
         .pipe(reload({ stream: true }));
 });
-
 gulp.task('browser-sync', () => {
     browserSync.init({
         server: '.'
