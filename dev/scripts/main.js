8e5aa056f47bffb5f225d5efee5c417986b21bdc
Eileen choi <eileen.h.choi@gmail.com>
2017-12-09 15:46:24 -0500

    tring smoothscroll

---
+++ dev/scripts/main.js
@@ -0,0 +1,12 @@
+
+//smoothscroll plugin
+
+$('a[href^="#"]').on('click', function (event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
