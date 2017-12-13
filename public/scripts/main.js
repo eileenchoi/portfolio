317d130ad6767e38195c2813ec1e1ee12d8ce4c2
Eileen choi <eileen.h.choi@gmail.com>
2017-12-13 09:58:17 -0500

    changed images

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,18 +11,3 @@ $('a[href^="#"]').on('click', function (event) {
         }, 1000);
     }
 });
-const tilt = $('.js-tilt').tilt();
-tilt.on('change', callback);  // parameters: event, transforms
-tilt.on('tilt.mouseLeave', callback); // parameters: event
-tilt.on('tilt.mouseEnter', callback); // parameters: event
-
-const tilt = $('.js-tilt').tilt();
-
-// Destroy instance
-tilt.tilt.destroy.call(tilt);
-
-// Get values of instance
-tilt.tilt.getValues.call(tilt); // returns [{},{},etc..]
-
-// Reset instance
-tilt.tilt.reset.call(tilt);
